8ca8fd1e78f7ec37f028328b5222db52b7fa6c09
carlosandre1055 <carlosandresilva1055@gmail.com>
2025-05-25 12:06:50 -0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname));
-
 const port = process.env.PORT || 3000;
+
+// Serve arquivos estáticos (CSS, JS, imagens, etc)
+app.use(express.static(path.join(__dirname)));
+
+// Rota raiz serve o painel.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'painel.html'));
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
